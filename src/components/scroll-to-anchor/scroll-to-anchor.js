72a6d0a0a1bdd865ec70fe8b20271ab5146bc99d
Uma <buldog52@gmail.com>
2019-10-17 22:01:59 +0300

    Added background to .delivery

--- src/components/scroll-to-anchor/scroll-to-anchor.js
+++ src/components/scroll-to-anchor/scroll-to-anchor.js
@@ -1,15 +1,21 @@
-// import $ from 'jquery';
-//
-// $('[href^="#"]').click(function(){
-//   const mainHeader = document.querySelector('.main-header');
-//   const mainHeaderPosition = getComputedStyle(mainHeader).position;
-//   const link = '#' + $(this).attr('data-id');
-//   let coordinates = $(link).offset().top;
-//
-//   // if ( mainHeaderPosition === 'fixed') {
-//   //   coordinates = $(link).offset().top-$('.main-header').height();
-//   // }
-//
-//   $('html, body').animate({scrollTop:coordinates}, 1000);
-//   return false;
-// });
+import $ from 'jquery';
+
+
+const screenHeight = $(window).height();
+const screenWidth = $(window).width();
+if ( screenWidth <= 991) {
+  $('[href^="#"]').click(function(){
+    const mainHeader = document.querySelector('.main-header');
+    const mainHeaderPosition = getComputedStyle(mainHeader).position;
+    const link = '#' + $(this).attr('data-id');
+    let coordinates = $(link).offset().top;
+
+    // if ( mainHeaderPosition === 'fixed') {
+    //   coordinates = $(link).offset().top-$('.main-header').height();
+    // }
+
+    $('html, body').animate({scrollTop:coordinates}, 1000);
+    return false;
+  });
+}
+
--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ import './components/grid/grid.less';
 import './components/fullpage/fullpage';
 import './components/menu-toggle/menu-toggle';
 import './components/scroll-up/scroll-up';
+import './components/scroll-to-anchor/scroll-to-anchor';
 import './app.scss';
